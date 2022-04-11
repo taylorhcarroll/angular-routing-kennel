8ef768c1db86e3fc300a815979b7b103457e138d
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2022-04-11 10:08:46 -0500

    added configuration for firesbase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,22 @@ import { HomeComponent } from './home/home.component';
 import { AnimalsComponent } from './animals/animals.component';
 import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 
+//allegedly, this is okay to expose due to FireBase apps being secured by writing [rules](/snippets/firestore-rules-recipes)
+//I'm storing this elsewhere though
+//not because I'm paranoid but because it's a good habit to keep up.
+const config = require('./config.json');
+const firebaseConfig = {
+
+  apiKey: config.firebaseConfig.apiKey,
+  authDomain: config.firebaseConfig.authDomain,
+  projectId: config.firebaseConfig.projectId,
+  storageBucket: config.firebaseConfig.storageBucket,
+  messagingSenderId: config.firebaseConfig.messagingSenderId,
+  appId: config.firebaseConfig.appId,
+  measurementId: config.firebaseConfig.measurementId
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
