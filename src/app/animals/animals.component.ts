39bfe41e64ce77a8dfbeba16fe0d2e5231a92355
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2022-04-24 15:27:52 -0500

    .

--- src/app/animals/animals.component.ts
+++ src/app/animals/animals.component.ts
@@ -7,7 +7,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore'
   styleUrls: ['./animals.component.sass']
 })
 export class AnimalsComponent implements OnInit {
-  animals$;
+  animals$: any;
   constructor(private afs: AngularFirestore) { }
 
   ngOnInit(): void {
