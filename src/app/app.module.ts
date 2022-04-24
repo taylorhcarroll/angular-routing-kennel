4a5c088eef25ebcd03fbef3e92b269597cdd7001
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2022-04-24 13:49:58 -0500

    added config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import firebaseConfig from './config.json'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,26 +15,19 @@ import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 //allegedly, this is okay to expose due to FireBase apps being secured by writing [rules](/snippets/firestore-rules-recipes)
 //I'm storing this elsewhere though
 //not because I'm paranoid but because it's a good habit to keep up.
-const config = require('./config.json');
-const firebaseConfig = {
+const config = {
 
-  apiKey: config.firebaseConfigDev.apiKey,
-  authDomain: config.firebaseConfigDev.authDomain,
-  projectId: config.firebaseConfigDev.projectId,
-  storageBucket: config.firebaseConfigDev.storageBucket,
-  messagingSenderId: config.firebaseConfigDev.messagingSenderId,
-  appId: config.firebaseConfigDev.appId,
-  measurementId: config.firebaseConfigDev.measurementId
+  apiKey: firebaseConfig.firebaseConfigDev.apiKey,
+  authDomain: firebaseConfig.firebaseConfigDev.authDomain,
+  projectId: firebaseConfig.firebaseConfigDev.projectId,
+  storageBucket: firebaseConfig.firebaseConfigDev.storageBucket,
+  messagingSenderId: firebaseConfig.firebaseConfigDev.messagingSenderId,
+  appId: firebaseConfig.firebaseConfigDev.appId,
+  measurementId: firebaseConfig.firebaseConfigDev.measurementId
 };
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AnimalsComponent,
-    AnimalDetailComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -41,6 +35,12 @@ const firebaseConfig = {
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
     AngularFireStorageModule // storage
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AnimalsComponent,
+    AnimalDetailComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
